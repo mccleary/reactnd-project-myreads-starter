c4f2e5173ed196332c929e13ae31bf3e3518c4d5
otsop110 <trjmard@gmail.com>
2017-11-21 09:14:39 +0100

    deleted assigning to new shelf

--- src/Book.js
+++ src/Book.js
@@ -4,12 +4,11 @@ import BookShelfChanger from './BookShelfChanger'
 
 class Book extends Component {
   static PropTypes = {
-    book: PropTypes.object.isRequired,
-    assignNewShelf: PropTypes.func.isRequired
+    book: PropTypes.object.isRequired
   }
 
   render() {
-    const { book, assignNewShelf } = this.props
+    const { book } = this.props
     const thumbnail = book.imageLinks.thumbnail
     const title = book.title
     const author = (book.authors || [])[0]
@@ -18,7 +17,7 @@ class Book extends Component {
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${thumbnail})`}}></div>
-          <BookShelfChanger book={book} assignNewShelf={assignNewShelf} />
+          <BookShelfChanger book={book} />
         </div>
         <div className="book-title">{title}</div>
         <div className="book-authors">{author}</div>
